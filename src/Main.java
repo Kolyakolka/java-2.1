17104c4914be8a86ffc01114ef9e701763e3c356
Kolyakolka <avr.var.02@mail.ru>
2022-06-23 21:06:32 +0500

    java2.1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountInKopecks = 1000000;
+        int percentInKopecks = 2000;
+        int bonus = amountInKopecks / percentInKopecks;
+        System.out.println("Nikolay, hello! Thank you for choosing our airline for your flight. Your bonus is:"+ bonus +" miles");
+    }
+}
